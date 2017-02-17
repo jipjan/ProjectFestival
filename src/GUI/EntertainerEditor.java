b87414dc78b23b9d405beded767374c3b1609c10
Thijs Wijnen <thijs.wijnen@gmail.com>
2017-02-17 12:12:49 +0100

    Added actionlisteners and it now is possible to get and set values.

--- src/GUI/EntertainerEditor.java
+++ src/GUI/EntertainerEditor.java
@@ -1,8 +1,14 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Events.Event;
+import Events.Time;
 
 
 /**
@@ -15,7 +21,10 @@ public class EntertainerEditor extends JPanel
 
     public static void main(String[] args)
     {
-        EntertainerEditor editor = new EntertainerEditor();
+        String testName = "TestName";
+        short popularity = 5;
+        Time time = new Time(10);
+        EntertainerEditor editor = new EntertainerEditor(new Event(testName,popularity,time));
 
         JFrame testFrame = new JFrame();
         testFrame.setTitle("testFrame");
@@ -26,42 +35,49 @@ public class EntertainerEditor extends JPanel
         //testFrame.setContentPane(editor);
         testFrame.setSize(300, 400);
         testFrame.setVisible(true);
+
     }
 
-    EntertainerEditor()
+    EntertainerEditor(Events.Event event)
     {
+
         super(new GridLayout(7,1));
         JPanel name = new JPanel(new FlowLayout());
         add(name);
         JLabel nameLbl = new JLabel("name:", SwingConstants.LEFT);
         name.add(nameLbl);
-        JTextField nameText = new JTextField("",20);
+        JTextField nameText = new JTextField(event.getName(),21);
         name.add(nameText);
+
+        boolean testing = true;
+
 
         JPanel time = new JPanel(new FlowLayout());
         add(time);
         JLabel timeLbl = new JLabel("entertain time:", SwingConstants.LEFT);
         time.add(timeLbl);
-        String[] comboTimes = { "1 hour", "2 hour", "3 hour", "4 hour", "5 hour" };
-        JComboBox timeCombo = new JComboBox(comboTimes);
-        timeCombo.setSelectedIndex(0);
-        timeCombo.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-
-            }
-        });
-        time.add(timeCombo);
+        JTextField timeText = new JTextField(String.valueOf(event.getTime().getDurationInMinutes()),16);
+        time.add(timeText);
 
 
         JPanel popularity = new JPanel(new FlowLayout());
         add(popularity);
         JLabel popLbl = new JLabel("popularity:", SwingConstants.LEFT);
         popularity.add(popLbl);
-        JSlider popSlider = new JSlider(0,100,4);
+        JSlider popSlider = new JSlider(0,10,event.getPopularity());
         popularity.add(popSlider);
+        JLabel popValue = new JLabel(String.valueOf(event.getPopularity()));
+        popularity.add(popValue);
+        popSlider.addChangeListener(new ChangeListener()
+        {
+            @Override
+            public void stateChanged(ChangeEvent e)
+            {
+                String value = String.valueOf(popSlider.getValue());
+                popValue.setText(value);
+            }
+        });
+
 
         JPanel empty1 = new JPanel(new FlowLayout());
         add(empty1);
@@ -80,7 +96,15 @@ public class EntertainerEditor extends JPanel
             @Override
             public void actionPerformed(ActionEvent e)
             {
-
+                event.set_name(nameText.getText());
+                event.set_popularity((short) popSlider.getValue());
+                event.set_time(new Time(Integer.valueOf(timeText.getText())));
+                if(testing == true)
+                {
+                    System.out.println(event.getName());
+                    System.out.println(event.getPopularity());
+                    System.out.println(event.getTime().getDurationInMinutes());
+                }
             }
         });
         save.add(saveBut);
