e518e445bcf9cc67df1bb455a35fe57761e50c26
Jaap-Jan de Wit <jipjan@gmail.com>
2017-03-13 09:35:40 +0100

    cell renderer

--- src/GUI/Agenda/PlanningTable.java
+++ src/GUI/Agenda/PlanningTable.java
@@ -2,6 +2,8 @@ package GUI.Agenda;
 
 import Events.Event;
 import GUI.CurrentSetup;
+import GUI.MyPanel.PlanningCellRenderer;
+import GUI.MyPanel.PlanningTableObject;
 import GUI.MyPanel.PlanningTableObjectModel;
 
 import javax.swing.*;
@@ -10,5 +12,7 @@ public class PlanningTable extends JTable {
     public PlanningTable(String... columns) {
         PlanningTableObjectModel<Event> m = new PlanningTableObjectModel<>(CurrentSetup.Events, columns);
         setModel(m);
+        setDefaultRenderer(PlanningTableObject.class, new PlanningCellRenderer());
+        setRowHeight(60);
     }
 }
--- src/GUI/MyPanel/PlanningCellRenderer.java
+++ src/GUI/MyPanel/PlanningCellRenderer.java
@@ -7,13 +7,14 @@ import java.awt.*;
 /**
  * Created by jaapj on 6-3-2017.
  */
-public class PlanningCellRenderer implements TableCellRenderer {
+public class PlanningCellRenderer extends JPanel implements TableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         PlanningTableObject obj = (PlanningTableObject) value;
-
-
-        return null;
+        setBackground(Color.PINK);
+        if (hasFocus) setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
+        else setBorder(null);
+        return this;
     }
 }
