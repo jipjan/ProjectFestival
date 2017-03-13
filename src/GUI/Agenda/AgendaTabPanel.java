95207bc0455e1179226636eb41a40dd783a1f92a
Jaap-Jan de Wit <jipjan@gmail.com>
2017-03-13 14:40:43 +0100

    opzet in timebars voor nieuw agenda plan systeem

--- src/GUI/Agenda/AgendaTabPanel.java
+++ src/GUI/Agenda/AgendaTabPanel.java
@@ -1,5 +1,7 @@
 package GUI.Agenda;
 
+import GUI.Agenda.Planning.SchedulingPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,8 @@ public class AgendaTabPanel extends JPanel {
     public AgendaTabPanel() {
         super(new GridLayout(1,1));
         add(new JScrollPane(new AgendaTable("Entertainer", "Tijd (in minuten)", "Populariteit")));
-        add(new JScrollPane(new PlanningTable("Tijd", "Podium 1", "Podium 2", "Podium 3")));
+        //add(new JScrollPane(new PlanningTable("Tijd", "Podium 1", "Podium 2", "Podium 3")));
+        add(new JScrollPane(new SchedulingPanel()));
     }
 
 }
--- src/GUI/Agenda/PlanningTable.java
+++ src/GUI/Agenda/PlanningTable.java
@@ -10,9 +10,13 @@ import javax.swing.*;
 
 public class PlanningTable extends JTable {
     public PlanningTable(String... columns) {
+        /*
         PlanningTableObjectModel<Event> m = new PlanningTableObjectModel<>(CurrentSetup.Events, columns);
         setModel(m);
         setDefaultRenderer(PlanningTableObject.class, new PlanningCellRenderer());
         setRowHeight(60);
+        */
+
+
     }
 }
