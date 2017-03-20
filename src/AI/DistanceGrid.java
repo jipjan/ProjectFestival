3a456332a7b64b5d3fe2d44ec518651382c5cc76
Jacco Steegman <Jacc.stee@gmail.com>
2017-03-20 11:34:25 +0100

    Added a class that will generate the DistanceGrid (not functional yet)

---
+++ src/AI/DistanceGrid.java
@@ -0,0 +1,37 @@
+package AI;
+
+import mapviewer.tiled.TileMap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jacco on 20/03/2017.
+ */
+public class DistanceGrid {
+    public ArrayList<ArrayList<Double>> distanceGrid;
+    private TileMap map;
+
+    DistanceGrid(TileMap map)
+    {
+        this.map = map;
+    }
+
+    private boolean isAccesable(int x, int y)
+    {
+        boolean[][] testArray =
+                {{true, true, true},
+                {true, true, true},
+                {true, true, true}};
+        return testArray[y][x];
+    }
+
+    private int getMapSizeX()
+    {
+        return 3;
+    }
+
+    private int getMapSizeY()
+    {
+        return 3;
+    }
+}
