1bb47a125c817e19e22b0c50edb2f67f6eea724d
Jaap-Jan de Wit <jipjan@gmail.com>
2017-04-08 11:11:37 +0200

    paring to fix output

--- src/Mapviewer/TiledMapReader/JsonClasses/TileData.java
+++ src/Mapviewer/TiledMapReader/JsonClasses/TileData.java
@@ -1,23 +1,26 @@
 package Mapviewer.TiledMapReader.JsonClasses;
 
-import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 /**
  * Created by Jaap-Jan on 8-4-2017.
  */
 public class TileData {
-    HashMap<Integer, Integer> _tiles;
+    HashMap<Double, Integer> _tiles;
 
     public TileData(int size) {
         _tiles = new HashMap<>(size);
     }
 
     public void addTile(int x, int y, int id) {
-        _tiles.put(x+y, id);
+        _tiles.put(paring(x, y), id);
     }
 
     public int getTileId(int x, int y) {
-        return _tiles.get(x+y);
+        return _tiles.get(paring(x, y));
+    }
+
+    private double paring(int x, int y) {
+        return 0.5*(x + y)*(x + y + 1) + y;
     }
 }
