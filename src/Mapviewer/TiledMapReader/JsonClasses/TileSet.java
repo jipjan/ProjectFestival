ddcb6293f4ab98e7ccbcfe714b22b1cc15f5cf50
Jaap-Jan de Wit <jipjan@gmail.com>
2017-04-08 11:51:22 +0200

    unused image

--- src/Mapviewer/TiledMapReader/JsonClasses/TileSet.java
+++ src/Mapviewer/TiledMapReader/JsonClasses/TileSet.java
@@ -20,7 +20,6 @@ public class TileSet {
     private int tilecount, tileheight;
     private transient HashMap<Integer, BufferedImage> imageTiles;
     private transient HashMap<Integer, Short[]> tiles = new HashMap<>();
-    private transient BufferedImage biImage;
     private int tilewidth;
 
     public int getColumns() {
