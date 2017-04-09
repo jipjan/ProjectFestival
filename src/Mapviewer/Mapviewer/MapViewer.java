4189b8945b0723a5c697c9e01314080ac7019f49
Jaap-Jan de Wit <jipjan@gmail.com>
2017-04-09 12:51:51 +0200

    rename variable

--- src/Mapviewer/Mapviewer/MapViewer.java
+++ src/Mapviewer/Mapviewer/MapViewer.java
@@ -1,12 +1,8 @@
 package Mapviewer.Mapviewer;
 
-import Mapviewer.TiledMapReader.JsonClasses.*;
 import Mapviewer.TiledMapReader.MyTiledJsonParser;
 import Mapviewer.Mapviewer.Drawers.TiledMapDrawer;
-import Mapviewer.Mapviewer.Drawers.DebugDraw;
 import Mapviewer.Mapviewer.Drawers.Draw;
-import NewAI.BaseClasses.MyBodies;
-import NewAI.BaseClasses.MyBody;
 import NewAI.BaseClasses.MyNpcWorld;
 
 import javax.swing.*;
@@ -20,22 +16,17 @@ import java.awt.geom.Point2D;
  * Created by Thijs on 20-2-2017.
  */
 public class MapViewer extends JPanel implements ActionListener {
-    private static final int AMOUNTOFNPCS = 50;
+    private static final int NPCs = 50;
 
     private TiledMapDrawer _map;
     private Camera _camera;
     private MyNpcWorld _world;
-
-
     private double lastTime = 0;
-
-    private MyBodies _myBodies;
-    private static Graphics2D g2d;
 
     public MapViewer() {
         _map = MyTiledJsonParser.jsonToTileMap("./resources/Festivalplanner Map V1 Test.json");
         _camera = new Camera(this, 1.0d, new Point2D.Double(_map.getWidth() / 2, _map.getHeight() / 2));
-        _world = new MyNpcWorld(AMOUNTOFNPCS, _map);
+        _world = new MyNpcWorld(NPCs, _map);
 
         new Timer(10, this).start();
     }
@@ -53,7 +44,7 @@ public class MapViewer extends JPanel implements ActionListener {
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        g2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D) g;
         Draw.drawGrid(this, _camera, g2d, 32);
         AffineTransform origin = g2d.getTransform();
         g2d.setTransform(this._camera.getTransform(getWidth(), getHeight()));
